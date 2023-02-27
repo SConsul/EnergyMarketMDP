c60f6f9ce0cb1dfe112a4f798f311bf86ef6e1d8
Paula Charles <91215001+paula-charles@users.noreply.github.com>
2023-02-27 15:49:15 -0800

    Correct some bugs in env.py

--- env.py
+++ env.py
@@ -10,8 +10,9 @@ class Market(object):
         self.num_panels = num_panels
 
     def init_day(self,episode):
-        self.cloudy = self.rng.randint(3,10)*self.rng.binomial(1, 0.1) #assume day is cloudy 10% of the time
+        #NW self.cloudy = self.rng.randint(3,10)*self.rng.binomial(1, 0.1) #assume day is cloudy 10% of the time
                                                                         #cloudy>0 ->cloudy (larger == more dense clouds)
+        self.cloudy = np.random.randint(3,10)*self.rng.binomial(1, 0.1)
         file_list = os.listdir("./data/data_demand")
         file_path = "./data/data_demand/"+file_list[episode]
         with open(file_path,'r') as f:
@@ -30,22 +31,23 @@ class Market(object):
         var = 4
         
         peak = 0.4*self.num_panels/(1+ self.cloudy) #on cloudy day, power can reduce to 10-25% normal (in kW), single panel is rated for 250-400W
-        power_supplied = np.exp(-((hour-12)**2/(2*var))) - np.exp(-(64/(2*var))) + np.rng.normal(0,0.2)
-        power_supplied = np.max(0,peak*power_supplied)
+        #NW power_supplied = np.exp(-((hour-12)**2/(2*var))) - np.exp(-(64/(2*var))) + np.rng.normal(0,0.2)
+        power_supplied = np.exp(-((hour-12)**2/(2*var))) - np.exp(-(64/(2*var))) + np.random.normal(0,0.2)
+        power_supplied = max(0,peak*power_supplied)
 
         return power_supplied
 
 
     def price_from_duck_curve(self, hour): #price is $/kWh
-        return 0.20 + 0.1*self.global_demand[hour]/22000.0
+        return 0.20 + 0.1*float(self.global_demand[hour])/22000.0
 
     def step(self,episode):
         if self.t == 24 or self.cloudy is None:
-            self.init_day()
+            self.init_day(episode)
 
         hour = self.t % 24
-        price = self.price_from_duck_curve(episode,hour)
-        power_supply = self.solar_power(episode, hour)
+        price = self.price_from_duck_curve(hour)
+        power_supply = self.solar_power(hour)
         
         self.t +=1
 
