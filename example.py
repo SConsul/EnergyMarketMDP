859d3fa2afe42d0c3f84906d5af298f3135ed851
Paula Charles <91215001+paula-charles@users.noreply.github.com>
2023-03-15 22:41:06 -0700

    Updated example with penalty

--- example.py
+++ example.py
@@ -1,12 +1,8 @@
-from der import DER
-from env import Market
-from agent import Agent
-import matplotlib.pyplot as plt
-
 market1 = Market(1)
-der1 = DER(power_capacity = 100 ,energy_capacity = 400)
+der1 = DER(power_capacity = 100 ,energy_capacity = 400, energy_demand = 100, h_demand=18)
 
-agent1 = Agent(nb_states=der1.nb_states(),nb_actions=der1.nb_actions())
+agent1 = Agent(nb_states=der1.nb_states(),nb_actions=der1.nb_actions(),
+               demand=der1.energy_dem,h_demand=der1.h_demand)
 list_hour, list_price, list_power_supplied, list_actions, list_s = agent1.Q_learning(24,market1,der1,1)
 
 fig, ax1 = plt.subplots()
@@ -18,9 +14,10 @@ ax1.tick_params(axis='y', labelcolor='r')
 
 ax2 = ax1.twinx()
 #ax2.set_ylabel('sin', color=color)
-ax2.plot(list_hour,list_power_supplied)
-ax2.plot(list_hour,list_actions)
-ax2.plot(list_hour,list_s)
+ax2.plot(list_hour,list_power_supplied,label='solar power')
+ax2.plot(list_hour,list_actions,label = 'actions')
+ax2.plot(list_hour,list_s, label = 'charging state')
 
 fig.tight_layout()
+plt.legend()
 plt.show()
